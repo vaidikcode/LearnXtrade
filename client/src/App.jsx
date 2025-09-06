1a162cbf0a9daadb0d998d28992b2db1e285cb8c
Harkirat Singh <singharkirath1511@gmail.com>
2025-09-07 00:10:40 +0530

    Add axios dependency and implement basic server setup with MongoDB connection; create User model and Dashboard page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,17 @@
 import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Dashboard from './pages/Dashboard'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-     
+     <Router>
+      <Routes>
+        <Route path='/' element={<Dashboard/>}></Route>
+      </Routes>
+     </Router>
     </>
   )
 }
